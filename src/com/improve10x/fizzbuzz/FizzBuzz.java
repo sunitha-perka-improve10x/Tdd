0b4cf6c3f37e30a3f6a2f6d09da5d3edea9661f3
sunitha <sunithapraveenkumar@gmail.com>
2023-07-19 12:13:29 +0530

    I completed FizzBuzz All testcases

--- src/com/improve10x/fizzbuzz/FizzBuzz.java
+++ src/com/improve10x/fizzbuzz/FizzBuzz.java
@@ -3,15 +3,17 @@ package com.improve10x.fizzbuzz;
 public class FizzBuzz {
     public String findFizzBuzz(int n) {
         String result = "";
-        if (n % 3 == 0 && n % 5 == 0) {
-            result = "FizzBuzz";
+        if (n > 0) {
+            if (n % 3 == 0 && n % 5 == 0) {
+                result = "FizzBuzz";
             } else if (n % 3 == 0) {
-            result = "Fizz";
+                result = "Fizz";
 
             } else if (n % 5 == 0) {
-            result = "Buzz";
-        } else {
-            result = Integer.toString(n);
+                result = "Buzz";
+            } else {
+                result = Integer.toString(n);
+            }
         }
         return result;
     }
--- src/com/improve10x/fizzbuzz/FizzBuzzTest.java
+++ src/com/improve10x/fizzbuzz/FizzBuzzTest.java
@@ -12,16 +12,44 @@ public class FizzBuzzTest {
     }
 
     @Test
-    public void givenFive_returnBuzz(){
+    public void givenFive_returnBuzz() {
         FizzBuzz fizzBuzz = new FizzBuzz();
         String five = fizzBuzz.findFizzBuzz(5);
-        assertEquals("Buzz",five);
+        assertEquals("Buzz", five);
     }
+
     @Test
-    public void givenThree_returnFizz(){
+    public void givenThree_returnFizz() {
         FizzBuzz fizzBuzz = new FizzBuzz();
         String Three = fizzBuzz.findFizzBuzz(3);
-        assertEquals("Fizz",Three);
+        assertEquals("Fizz", Three);
+    }
 
+    @Test
+    public void givenFive_returnB() {
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        String NegativeFive = fizzBuzz.findFizzBuzz(-5);
+        assertEquals("Buzz", NegativeFive);
+    }
+
+    @Test
+    public void givenZero_returnEmpty() {
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        String zero = fizzBuzz.findFizzBuzz(0);
+        assertEquals("", zero);
+    }
+
+    @Test
+    public void givenMinusTen_returnMinusTen() {
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        String ten = fizzBuzz.findFizzBuzz(0);
+        assertEquals("", ten);
+    }
+
+    @Test
+    public void givenFifteen_returnFizzBuzz() {
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        String fifteen = fizzBuzz.findFizzBuzz(15);
+        assertEquals("FizzBuzz", fifteen);
     }
 }

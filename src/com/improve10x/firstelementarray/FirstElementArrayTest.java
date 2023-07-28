78f121db0f46f54f82eceb2bba84af454769e35b
sunitha <sunithapraveenkumar@gmail.com>
2023-07-28 17:22:29 +0530

    I completed givenThreeElements returnFirstElements Testcase is passed

--- src/com/improve10x/firstelementarray/FirstElementArrayTest.java
+++ src/com/improve10x/firstelementarray/FirstElementArrayTest.java
@@ -34,6 +34,17 @@ public class FirstElementArrayTest {
         int oneTwoThree = firstElement.FindFirstElement(new int[]{1, 2, 3});
         assertEquals(1, oneTwoThree);
     }
-
+    @Test
+    public void given80and5and100_return80(){
+        FirstElement firstElement = new FirstElement();
+        int values = firstElement.FindFirstElement(new int[]{80, 5, 100});
+        assertEquals(80, values);
+    }
+    @Test
+    public void givenMinus500and0and50_returnMinus500(){
+        FirstElement firstElement = new FirstElement();
+        int values = firstElement.FindFirstElement(new int[]{-500, 0, 50});
+        assertEquals(-500, values);
+    }
 }
 

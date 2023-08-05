bd9e055891c1a8721b7d842c4ba042f2ce8182fc
sunitha <sunithapraveenkumar@gmail.com>
2023-08-05 15:19:46 +0530

    I completed givenEmpty_returnZero

--- src/com/improve10x/largestNumberArray/LargestNumberArrayTest.java
+++ src/com/improve10x/largestNumberArray/LargestNumberArrayTest.java
@@ -3,6 +3,9 @@ package com.improve10x.largestNumberArray;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+
 public class LargestNumberArrayTest {
     @Test
     public void nothing(){
@@ -12,7 +15,13 @@ public class LargestNumberArrayTest {
     public void givenZero_returnZero(){
     LargestNumber largestNumber = new LargestNumber();
     int zero = largestNumber.findLargestNumber(new int[]{0});
-        Assertions.assertEquals(0,zero);
+        assertEquals(0,zero);
+    }
+    @Test
+    public void givenEmpty_returnNull(){
+        LargestNumber largestNumber = new LargestNumber();
+        int empty = largestNumber.findLargestNumber(new int[]{});
+        assertEquals(empty, empty);
     }
 
 

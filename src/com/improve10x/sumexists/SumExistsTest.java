aa6c0f4e3f2ce404844ca9636a77d916dca984d1
sunitha <sunithapraveenkumar@gmail.com>
2023-08-08 15:50:21 +0530

    given null return no

--- src/com/improve10x/sumexists/SumExistsTest.java
+++ src/com/improve10x/sumexists/SumExistsTest.java
@@ -1,11 +1,18 @@
 package com.improve10x.sumexists;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class SumExistsTest {
 
     @Test
     public void nothing(){
+    }
 
+    @Test
+    public void givenNull_returnNo(){
+        Sumexists sumexists = new Sumexists();
+        String sumOfTwo = sumexists.findSumofExists(null,0);
+        Assertions.assertEquals("no",sumOfTwo);
     }
 }
---
+++ src/com/improve10x/sumexists/Sumexists.java
@@ -0,0 +1,8 @@
+package com.improve10x.sumexists;
+
+public class Sumexists {
+
+    public String findSumofExists(Object o, int i) {
+        return "no";
+    }
+}
